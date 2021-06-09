b69db0e15941d0bf000f2dedd09f8513a04f5760
vyudin <vyudin@tsconsulting.com>
2021-06-09 11:53:50 +0300

    test diapason

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,20 +1,20 @@
 fun main() {
     val name = "Madrigal"
-    val healthPoints = 85
+    val healthPoints = 88
     val isBlessed = true
     val isImmortal = false
 
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println(auraColor)
+    println("AURA: $auraColor")
 
     val healthStatus = if (healthPoints == 100) {
         "is in excellent condition!"
-    } else if (healthPoints >= 90) {
+    } else if (healthPoints in 90..99) {
         "has a few scratches."
-    } else if (healthPoints >= 75) {
+    } else if (healthPoints in 75..89) {
         if (isBlessed) "has some minor wounds but is healing quite quickly!" else "has some minor wounds."
-    } else if (healthPoints >= 15) {
+    } else if (healthPoints in 15..74) {
         "looks pretty hurt."
     } else {
         "is in awful condition!"
@@ -22,6 +22,6 @@ fun main() {
 
     // Состояние игрока
     println(name + " " + healthStatus)
-
+    println("$name $healthStatus")
 
 }
