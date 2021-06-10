8cf86956cfeef2022ff88636dd27261406ca3c76
vyudin <vyudin@tsconsulting.com>
2021-06-10 19:20:46 +0300

    start work with functions

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -4,28 +4,7 @@ fun main() {
     val isBlessed = true
     val isImmortal = false
 
-//    val healthStatus = if (healthPoints == 100) {
-//        "is in excellent condition!"
-//    } else if (healthPoints in 90..99) {
-//        "has a few scratches."
-//    } else if (healthPoints in 75..89) {
-//        if (isBlessed) "has some minor wounds but is healing quite quickly!" else "has some minor wounds."
-//    } else if (healthPoints in 15..74) {
-//        "looks pretty hurt."
-//    } else {
-//        "is in awful condition!"
-//    }
-
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> when (isBlessed) {
-            true -> "has some minor wounds but is healing quite quickly!"
-            false -> "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
     // Состояние игрока
     val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
@@ -44,4 +23,18 @@ fun main() {
     }
     println("Race: $race. Faction: $faction")
 
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> when (isBlessed) {
+            true -> "has some minor wounds but is healing quite quickly!"
+            false -> "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+    return healthStatus
 }
