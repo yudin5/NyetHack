e2d129fd30e6089f3cb4fce1fb2d60b49abc7560
vyudin <vyudin@tsconsulting.com>
2021-06-10 20:01:54 +0300

    work with functions, default args, named args

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -4,29 +4,50 @@ fun main() {
     val isBlessed = true
     val isImmortal = false
 
+    // Аура
+    val auraColor = getAuraColor(isBlessed, healthPoints, isImmortal)
     val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
     // Состояние игрока
-    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println("(Aura: $auraColor) " +
-    "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
 
-    val race = "gnome"
-    val faction = when (race) {
-        "dwarf" -> "Keepers of the Mines"
-        "gnome" -> "Keepers of the Mines"
-        "orc" -> "Free People of the Rolling Hills"
-        "human" -> "Free People of the Rolling Hills"
-        else -> "Strange people..."
-    }
-    println("Race: $race. Faction: $faction")
+    printPlayerStatus(
+        auraColor = "NONE",
+        isBlessed = true,
+        name = "Madrigal",
+        healthStatus = healthStatus
+    )
+
+    castFireball(5)
+    castFireball()
 
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor) " +
+                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+    )
+    println("$name $healthStatus")
+}
+
+private fun getAuraColor(
+    isBlessed: Boolean,
+    healthPoints: Int,
+    isImmortal: Boolean
+): String {
+    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> when (isBlessed) {
@@ -36,5 +57,6 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-    return healthStatus
-}
+
+private fun castFireball(numFireballs: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
--- src/main/kotlin/exercises/chapter_3/Game_ConditionalIsChallenges.kt
+++ src/main/kotlin/exercises/chapter_3/Game_ConditionalIsChallenges.kt
@@ -1,50 +1,49 @@
 package exercises.chapter_3
 
-import kotlin.math.pow
-
 fun main() {
-    val name = "Estragon"
-    val healthPoints = 88
-    val isBlessed = true
-    val isImmortal = false
-
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> when (isBlessed) {
-            true -> "has some minor wounds but is healing quite quickly!"
-            false -> "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
-
-    // Состояние игрока
-    val karma = (Math.random().pow((110 - healthPoints) / 100.0) * 20).toInt()
-    val auraColor = when (karma) {
-        in 0..5 -> "RED"
-        in 6..10 -> "ORANGE"
-        in 11..15 -> "PURPLE"
-        in 16..20 -> "GREEN"
-        else -> "NONE"
-    }
-    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
-
-    println("(Aura: ${if (auraVisible) auraColor else "NONE"}) " +
-    "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
-
-    // val statusFormatString = "(HP)(A) -> H"
-    val statusFormatString = "(HP: $healthPoints)(Aura: $auraColor) -> $healthStatus"
-    println("statusFormatString = $statusFormatString")
-
-    val race = "gnome"
-    val faction = when (race) {
-        "dwarf" -> "Keepers of the Mines"
-        "gnome" -> "Keepers of the Mines"
-        "orc" -> "Free People of the Rolling Hills"
-        "human" -> "Free People of the Rolling Hills"
-        else -> "Strange people..."
-    }
+//    val name = "Estragon"
+//    val healthPoints = 88
+//    val isBlessed = true
+//    val isImmortal = false
+//
+//    val healthStatus = when (healthPoints) {
+//        100 -> "is in excellent condition!"
+//        in 90..99 -> "has a few scratches."
+//        in 75..89 -> when (isBlessed) {
+//            true -> "has some minor wounds but is healing quite quickly!"
+//            false -> "has some minor wounds."
+//        }
+//        in 15..74 -> "looks pretty hurt."
+//        else -> "is in awful condition!"
+//    }
+//
+//    // Состояние игрока
+//    val karma = (Math.random().pow((110 - healthPoints) / 100.0) * 20).toInt()
+//    val auraColor = when (karma) {
+//        in 0..5 -> "RED"
+//        in 6..10 -> "ORANGE"
+//        in 11..15 -> "PURPLE"
+//        in 16..20 -> "GREEN"
+//        else -> "NONE"
+//    }
+//    val auraVisible = (isBlessed && healthPoints > 50) || isImmortal
+//
+//    println("(Aura: ${if (auraVisible) auraColor else "NONE"}) " +
+//    "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+//    println("$name $healthStatus")
+//
+//    // val statusFormatString = "(HP)(A) -> H"
+//    val statusFormatString = "(HP: $healthPoints)(Aura: $auraColor) -> $healthStatus"
+//    println("statusFormatString = $statusFormatString")
+//
+//    val race = "gnome"
+//    val faction = when (race) {
+//        "dwarf" -> "Keepers of the Mines"
+//        "gnome" -> "Keepers of the Mines"
+//        "orc" -> "Free People of the Rolling Hills"
+//        "human" -> "Free People of the Rolling Hills"
+//        else -> "Strange people..."
+//    }
+//    println("Race: $race. Faction: $faction")
 
 }
